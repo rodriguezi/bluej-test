f6c9d4bac80a47c699f301dacdfac2c46926babd
Ivonne Rodriguez <rodriguezi140845@student.vvc.edu>
2020-04-25 16:33:07 -0700

    This is an update

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main
 {
  public static void main(String[] args) 
  {
-  System.out.print("Hello, World");
+  System.out.println("Hello, World");
+  System.out.println("My name is Ivonne rodriguez");
  }
 }
